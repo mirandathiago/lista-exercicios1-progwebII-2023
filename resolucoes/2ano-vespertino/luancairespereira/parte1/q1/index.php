b33653cd1186286bd98ffd03ade156d421719f57
Luan Caires <113989560+luan-caires@users.noreply.github.com>
2023-04-16 10:32:19 -0300

    fiz q questão 1 da parte 1 da lista

---
+++ resolucoes/2ano-vespertino/luancairespereira/parte1/q1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Questão1</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<header>
+		<h1>Formulário de cálculo de desconto de 10%</h1>
+	</header>
+	<div class="container">
+		<div class="box formulario">
+			<h2>Entre em contato</h2>
+			<form action="resposta.php" method="post">
+				<label>Informe o preço do produto:
+					<input type="number" id="nome" name="preco" required>
+				</label>
+				<button name="enviar"> Enviar </button>
+			</form>
+		</div>		
+	</div>
+</body>
+</html>
---
+++ resolucoes/2ano-vespertino/luancairespereira/parte1/q1/resposta.php
@@ -0,0 +1,27 @@
+<?php
+	$preco = $_POST["preco"]??0;
+	$desconto = 0.01;
+	$precofinal = $preco * $desconto;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Questão 1</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<header>
+		<h1>Formulário de cálculo de desconto de 10%</h1>
+	</header>
+	<div class="container">
+		<div class="box resposta">
+			<h2>Preços finais</h2>
+			<p>o Valor de entrada R$<?php $preco?> com o desconto de <?php $desconto?> ficará no valor de <?php $precofinal?>.</p>
+            <a href="" class="link">Voltar</a>
+		</div>
+	</div>
+</body>
+</html>
