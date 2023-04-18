7705c4ced91d90a434a7fe3ebccdd8e4ebf8264e
Felipe Ribeiro De Souza <felipemecanicosouza@gmail.com>
2023-04-18 18:20:39 -0300

    Questão 4 da segunda parte da lista resolvida

---
+++ resolucoes/2ano-matutino/FelipeRibeirodeSouza/parte2/q4/index.php
@@ -0,0 +1,66 @@
+<?php
+	$nA=$_POST["nA"] ?? 1;
+	$nB=$_POST["nB"] ?? 1;
+	$nC=$_POST["nC"] ?? 1;
+
+	$delta = ($nB ** 2) - (4*$nA*$nC);
+
+	$raizum= (-$nB + sqrt($delta))/(2*$nA);
+
+	$raizdois= (-$nB - sqrt($delta))/(2*$nA);
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Formulário</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<header> 
+		<h1>Calculo das raízes de uma equação de segundo-grau:</h1>
+	</header>
+	<div class="container">
+		<div class="box formulario">
+			<h2>Entre em contato</h2>
+			<form action="index.php" method="post">
+				<label>Informe o valor A da equação:
+					<input type="number" id="numero" name="nA" required step="0.1" value="1">
+				</label>
+
+				<label>Informe o valor B da equação:
+					<input type="number" id="numero" name="nB" required step="0.1" value="1">
+				</label>
+
+				<label>Informe o valor C da equação:
+					<input type="number" id="numero" name="nC" required step="0.1" value="1">
+				</label>
+
+				<button name="enviar"> Calcular </button>
+			</form>
+		</div>
+		<div class="box resposta">
+			<h2>Resposta</h2>
+			<?php
+				$metodo = $_SERVER["REQUEST_METHOD"];
+				if($metodo =="POST"){
+					if($delta < 0){
+						echo"<p class='alerta-vermelho'>A equação não possui raizes reais</p>";
+					}else if($delta == 0){
+						echo"<p class='alerta-verde'>A equação possui apenas 1 raiz real:\n{$raizum}</p>";
+					}else{
+						echo"<p class='alerta-verde'>A equação possui duas raizes reais:\nx1={$raizum} e x2={$raizdois}</p>";
+					}
+				}else{
+					echo "<p class='alerta-vermelho";
+				}
+			?>
+            <a href="" class="link">Voltar</a>
+		</div>
+	</div>
+</body>
+</html>
