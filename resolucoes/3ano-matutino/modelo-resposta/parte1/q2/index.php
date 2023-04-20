5de7b001361a308d6ce24b83defbefd129b3a066
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2023-04-20 13:45:53 -0300

    Part 1 Accomplished ✔

--- resolucoes/3ano-matutino/modelo-resposta/parte1/q2/index.php
+++ resolucoes/3ano-matutino/modelo-resposta/parte1/q2/index.php
@@ -2,20 +2,20 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Preço com Desconto</title>
+	<title>Formulário</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Preço com Desconto</h1>
+		<h1>Dobro e Metade</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre com os valores</h2>
+			<h2>Dobro e Metade</h2>
 			<form action="resposta.php" method="post">
 				<label>Valor:
-					<input type="number" step="0.01" required id="valor" name="valor" >
+					<input type="text" id="valor" name="valor" step=".1" required>
 				</label>
 
 				<button name="enviar"> Enviar </button>
--- resolucoes/3ano-matutino/modelo-resposta/parte1/q2/resposta.php
+++ resolucoes/3ano-matutino/modelo-resposta/parte1/q2/resposta.php
@@ -1,9 +1,8 @@
 <?php
 
-	if(isset($_POST['enviar'])) {
-		$preco_original = $_POST['valor'];
-		$preco_final = $preco_original - ($preco_original * 0.1); // Desconto de 10%
-	}
+	$valor = $_POST["valor"];
+	$dobro = $valor * 2;
+	$metade = $valor / 2;
 
 ?>
 
@@ -11,18 +10,19 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Preço com Desconto</title>
+	<title>Dobro e Metade</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Preço com Desconto</h1>
+		<h1>Resposta</h1>
 	</header>
 	<div class="container">
 		<div class="box resposta">
-			<h2>Valor com Desconto</h2>
-			<p class="alerta-verde">Preço com desconto: R$ <?= number_format($preco_final, 2, ',', '.'); ?></p>
+			<h2>Resposta</h2>
+			<p class="alerta-verde">Dobro: <?= number_format($dobro, 2, ',', '.') ?></p>
+			<p class="alerta-vermelho">Metade: <?= number_format($metade, 2, ',', '.') ?></p>
             <a href="index.php" class="link">Voltar</a>
 		</div>
 	</div>
