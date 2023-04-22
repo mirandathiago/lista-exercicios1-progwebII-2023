bbbec4479a0178287e4cc0e5db667d363ac4c118
Kayllane Santos <kayllanehss@gmail.com>
2023-04-22 11:24:45 -0300

    Resolução da Questão 8 da Parte 1.

--- resolucoes/3ano-matutino/kayllane-santos/parte1/q8/index.php
+++ resolucoes/3ano-matutino/kayllane-santos/parte1/q8/index.php
@@ -3,65 +3,43 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Formulário</title>
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>MAGAZINE PHP</h1>
 	</header>
 	<div class="container">
+
+		<!-- Maria precisa calcular o valor total que ela irá pagar pela compra de um celular no cartão na loja Magazine PHP. Para ajudá-la, crie um formulário que receba o valor do celular e em quantas vezes o produto será dividido, podendo ser de 0 a 12x, sendo que 0 seria a compra à vista. -->
+
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
+			<h2>Preencha os dados</h2>
+			<form action="resposta.php" method="POST">
+				<label>Valor do celular:
+					<input type="number" id="valor" name="valor" step="0.1" required>
 				</label>
 
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
+				<label>Deseja dividir em quantas vezes:
+					<select name="vezes">
+						<option value="">Selecione...</option>
+						<option value="1">0x</option>
+						<option value="2">2x</option>
+						<option value="3">3x</option>
+						<option value="4">4x</option>
+						<option value="5">5x</option>
+						<option value="6">6x</option>
+						<option value="7">7x</option>
+						<option value="8">8x</option>
+						<option value="9">9x</option>
+						<option value="10">10x</option>
+						<option value="11">11x</option>
+						<option value="12">12x</option>
 					</select>
 			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
-				</label>
 
 				<button name="enviar"> Enviar </button>
 			</form>
