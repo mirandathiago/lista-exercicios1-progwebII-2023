5fb70bca0f9554b3bcae60dc3e2a9b172569b2ef
Kayllane Santos <kayllanehss@gmail.com>
2023-04-22 16:04:50 -0300

    Resolução da Questão 8 da Parte 2.

--- resolucoes/3ano-matutino/kayllane-santos/parte2/q8/index.php
+++ resolucoes/3ano-matutino/kayllane-santos/parte2/q8/index.php
@@ -1,71 +1,74 @@
+<?php
+	/*
+	Calcule o IMC (Índice de Massa Corporal) e exiba em qual faixa de peso a pessoa se encontra. Utilize as seguintes faixas: abaixo do peso (IMC menor que 18,5), peso normal (IMC entre 18,5 e 24,9), sobrepeso (IMC entre 25 e 29,9), obesidade grau 1 (IMC entre 30 e 34,9), obesidade grau 2 (IMC entre 35 e 39,9) e obesidade grau 3 (IMC igual ou maior que 40).
+	O cálculo do IMC é realizado pela fórmula: IMC = peso / (altura^2), onde peso é a massa corporal em quilogramas e altura é a altura em metros elevada ao quadrado.
+	Utilize condicionais compostas com operadores lógicos para verificar em qual faixa de peso a pessoa se encontra.
+	*/
+
+	$peso = $_POST["peso"] ?? 40;
+	$altura = $_POST["altura"] ?? 1.56;
+	$imc = $peso/($altura**2);
+
+	$imc = number_format($imc, 2);
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Formulário</title>
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Cálculo de IMC</h1>
 	</header>
 	<div class="container">
+		<!-- Crie um formulário que permita ao usuário informar o peso e a altura. -->
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
+			<h2>Informe</h2>
+			<form action="index.php" method="POST">
+				<label>Seu Peso:
+					<input type="number" name="peso" step="0.01" value="<?=$peso?>" required>
 				</label>
 
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
+				<label>Sua Altura:
+					<input type="number" name="altura" step="0.01" value="<?=$altura?>" required>
 				</label>
 
 				<button name="enviar"> Enviar </button>
+
 			</form>
-		</div>		
+		</div>	
+		
+		<div class="box resposta">
+			<h2>Resultado</h2>
+			<?php
+			$metodo = $_SERVER["REQUEST_METHOD"];
+
+			if($metodo == "POST"){
+				if($imc < 18.5){
+					echo "<p class='alerta-amarelo'>Seu IMC é {$imc}. Abaixo do peso.</p>";
+				}else if($imc >= 18.5 && $imc <= 24.9){
+					echo "<p class='alerta-verde'>Seu IMC é {$imc}. Peso normal.</p>";
+				}else if($imc >= 25 && $imc <= 29.9){
+					echo "<p class='alerta-azul'>Seu IMC é {$imc}. Sobrepeso.</p>";
+				}else if($imc >= 30 && $imc <= 34.9){
+					echo "<p class='alerta-rosa'>Seu IMC é {$imc}. Obesidade Grau 1.</p>";
+				}else if($imc >= 35 && $imc <= 39.9){
+					echo "<p class='alerta-roxo'>Seu IMC é {$imc}. Obesidade Grau 2.</p>";
+				}else{
+					echo "<p class='alerta-vermelho'>Seu IMC é {$imc}. Obesidade Grau 3</p>";
+				}
+			}else{
+				echo "<p>Aguardando a Operação</p>";
+			}
+			?>
+            
+		</div>
 	</div>
 </body>
 </html>
