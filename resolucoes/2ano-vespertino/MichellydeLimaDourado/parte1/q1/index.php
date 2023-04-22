b7a04515ec96278e68435aa69f5e5cb4e3c505cd
Michelly Lima <michellylimad1@gmail.com>
2023-04-21 23:51:08 -0300

    Resolução da questão 1 da parte 1

--- resolucoes/2ano-vespertino/MichellydeLimaDourado/parte1/q1/index.php
+++ resolucoes/2ano-vespertino/MichellydeLimaDourado/parte1/q1/index.php
@@ -2,70 +2,39 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Formulário</title>
+	<title>OperadoresAritmeticos</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Desconto</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
-				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
+		<form action="index.php" method="post">
+				<label>Valor do porduto:
+					<input type="number" id="preco" name="preco" required>
 				</label>
 
 				<button name="enviar"> Enviar </button>
 			</form>
 		</div>		
+	</div>
+	<div class="container">
+		<div class="box resposta">
+			<h2>Resposta</h2>
+			
+			<?php
+			
+				$preco = $_POST["preco"] ?? 10;
+				$desconto = $preco * (10/100);
+				$valorf =  $preco - $desconto;
+			
+			?>
+			<p>De R$<?=$preco?> com 10% de desconto, pagará R$<?=$valorf?></p>
+			
+		</div>
 	</div>
 </body>
 </html>
