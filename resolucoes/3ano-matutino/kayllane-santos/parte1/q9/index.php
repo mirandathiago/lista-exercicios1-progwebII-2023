dafdc810abd7dbc8e9e570c5fef87cc7f97d6024
Kayllane Santos <kayllanehss@gmail.com>
2023-04-22 12:19:22 -0300

    Resolução da Questão 9 da Parte 1.

--- resolucoes/3ano-matutino/kayllane-santos/parte1/q9/index.php
+++ resolucoes/3ano-matutino/kayllane-santos/parte1/q9/index.php
@@ -1,71 +1,54 @@
+<?php
+	// Em seguida, crie um código em PHP que calcule o valor em dólar correspondente e exiba na tela.
+	$valoremreal = $_POST["valorreal"] ?? 1;
+	$cotacao = $_POST["cotacao"] ?? 1;
+	
+	$valoremdolar = $valoremreal/$cotacao;
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Formulário</title>
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Conversor de Real para Dólar</h1>
 	</header>
 	<div class="container">
+		<!-- João está planejando uma viagem para o exterior e precisa converter o valor do real para dólar. Crie um formulário que receba o valor em real e a cotação do dólar em reais. -->
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
+		<h2>Preencha</h2>
+			<form action="index.php" method="POST">
+				<label>Valor em Real:
+					<input type="number" name="valorreal" value="<?=$valoremreal?>" step="0.1" required>
 				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
+				<label>Cotação atual do dólar(em reais):
+					<input type="number" name="cotacao" value="<?=$cotacao?>" step="0.1" required>
 				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
-				</label>
-
 				<button name="enviar"> Enviar </button>
 			</form>
-		</div>		
+
+		</div>	
+		
+		<div class="box resposta">
+			<h2>Valor em Dólar: </h2>
+			<?php
+				$metodo = $_SERVER["REQUEST_METHOD"];
+
+				if($metodo == "POST"){
+					echo "<p class='alerta-azul'>" . number_format($valoremdolar, 2) . " US$</p>";
+				}else{
+					echo "<p>Aguardando a Operação</p>";
+				}
+			?>
+            
+		</div>
 	</div>
 </body>
 </html>
