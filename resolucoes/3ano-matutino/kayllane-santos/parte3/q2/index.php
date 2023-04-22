9ad4f8c4cd41c4e7e89f478cd38896087f85e3dd
Kayllane Santos <kayllanehss@gmail.com>
2023-04-22 17:30:51 -0300

    Resolução da Questão 2 da Parte 3.

--- resolucoes/3ano-matutino/kayllane-santos/parte3/q2/index.php
+++ resolucoes/3ano-matutino/kayllane-santos/parte3/q2/index.php
@@ -3,69 +3,28 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Formulário</title>
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Tabuada</h1>
 	</header>
 	<div class="container">
+		<!--  Desenvolva um programa que receba como entrada um número inteiro e calcule a sua tabuada de multiplicação utilizando o comando WHILE. Exiba a tabuada na tela. -->
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
-				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
+			<form action="resposta.php" method="POST">
+				<h2>Vamos calcular a tabuada de multiplicação</h2>
+				<label>Insira um número inteiro:
+					<input type="number" name="numero" required>
 				</label>
 
 				<button name="enviar"> Enviar </button>
 			</form>
-		</div>		
+		</div>	
+
 	</div>
 </body>
 </html>
--- resolucoes/3ano-matutino/kayllane-santos/parte3/q2/resposta.php
+++ resolucoes/3ano-matutino/kayllane-santos/parte3/q2/resposta.php
@@ -1,24 +1,42 @@
+<?php
+	$numero = $_POST["numero"] ?? 1;
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Formulário</title>
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Tabuada</h1>
 	</header>
 	<div class="container">
-		<div class="box resposta">
-			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
-		</div>
+	<div class="box resposta">
+			<h2>Tabuada do <?=$numero?></h2>
+
+			<?php
+				
+				$resultado = 1;
+				$i = 1;
+
+				$metodo = $_SERVER["REQUEST_METHOD"];
+				if($metodo == "POST"){
+					while($i <= 10){
+						$resultado = $numero * $i;
+						echo "<p class='alerta-azul'>{$numero} x {$i} = {$resultado}. </p>";
+						$i++;
+					}
+				}else{
+					echo "<p>Aguardando a Operação</p>";
+				}
+			?>
+
+		</div>	
 	</div>
 </body>
 </html>
