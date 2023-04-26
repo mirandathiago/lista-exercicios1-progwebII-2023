8d5f14a61efc5765e2b5e18cd58d6bceaf822408
iLevsPHP_ <202215210015@ifba.edu.br>
2023-04-25 23:39:21 -0300

    Finalizada questão 1

--- modelo-resposta/parte1/q1/questao-1.php
+++ modelo-resposta/parte1/q1/questao-1.php
@@ -8,71 +8,28 @@
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Minha Loja/Nossa Loja</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
+			<h2>A nossa loja te dá 10% de desconto:</h2>
+			<form action="questao-1.php" method="post">
+				<label>Valor da compra:
+					<input type="number" id="precoin" name="precoin" required>
 				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
-				</label>
-
 				<button name="enviar"> Enviar </button>
 			</form>
 		</div>
 		<div class="box resposta">
-			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
+			<h2>O preço final será:</h2>
+			<?php
+				if ($_SERVER["REQUEST_METHOD"]=="POST") {
+				
+					$precoinicial = $_POST["precoin"];
+					$precofinal = $precoinicial * 0.9;
+					echo "<p class='alerta-vermelho'>O valor com o desconto oferecido é de R$ {$precofinal}</p>";
+				}
+			?>
 		</div>
 	</div>
 </body>
