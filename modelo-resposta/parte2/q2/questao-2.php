9756f171bec77e43130a52433f2da1fdbd35815e
iLevsPHP_ <202215210015@ifba.edu.br>
2023-04-27 00:43:17 -0300

    COMECEI A QUESTÃO 2

---
+++ modelo-resposta/parte2/q2/questao-2.php
@@ -0,0 +1,44 @@
+<?php
+	$consumo = $_POST["KWH"] ?? 1;
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Formulário</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<header>
+		<h1>Formulário de Contato</h1>
+	</header>
+	<div class="container">
+		<div class="box formulario">
+			<h2>Entre em contato</h2>
+			<form action="questao-2.php" method="post">
+				<label>Indique o consumo de energia:
+					<input type="number" id="kwh" name="kwh" required>
+				</label>
+				<button name="enviar"> Enviar </button>
+			</form>
+		</div>
+		<div class="box resposta">
+			<h2>Resposta</h2>
+			<?php
+
+			if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+
+				echo "<p class='alerta-amarelo'></p>";
+			}
+
+
+			?>
+		</div>
+	</div>
+</body>
+</html>
