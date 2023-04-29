e1a562b98e1ca190aa45397cb9d32b6323235806
pedroagno <114294264+pedroagno@users.noreply.github.com>
2023-04-29 11:42:47 -0300

    Questão 8 da parte 3 realizada, ok!

--- resolucoes/2ano-matutino/pedro-agno/parte3/q8/index.php
+++ resolucoes/2ano-matutino/pedro-agno/parte3/q8/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,56 +14,13 @@
 	<div class="container">
 		<div class="box formulario">
 			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
-				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
-				</label>
-
+			<form action="resposta.php" method="post">
+				<label>numerohabitantesA:
+					<input type="number" id="nome" name="numerohabitantesA" required>taxaA
+					<input type="number" id="nome" name="taxaA" required>numerohabitantesB
+					<input type="number" id="nome" name="numerohabitantesB" required>taxaB
+					<input type="number" id="nome" name="taxaB" required>
+			</label>
 				<button name="enviar"> Enviar </button>
 			</form>
 		</div>		
