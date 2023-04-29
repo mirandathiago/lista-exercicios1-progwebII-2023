ad6b9e3fd010ed3450c45ba7ae28b1265f3d56c3
niculox <132169745+niculox@users.noreply.github.com>
2023-04-29 19:28:05 -0300

    Update index.php

--- resolucoes/2ano-vespertino/nicolas/parte1/q6/index.php
+++ resolucoes/2ano-vespertino/nicolas/parte1/q6/index.php
@@ -1,3 +1,19 @@
+
+<?php
+
+    /*6- João irá comprar uma terreno para construir sua casa e para isso gostaria de calcular a área total desse terreno, 
+    crie um formulário que receba a largura e o comprimento do terreno, em seguida crie um código 
+    PHP que irá calcular esta área e exibir na tela.
+    Entrada: largura: 10m, comprimento: 20m
+    Saída esperada: 200m2
+    */
+
+    $largura = $_POST["largura"] ?? 0; // a variavel recebe o valor utilizando o metodo post do formulario
+    $comprimento = $_POST["comprimento"] ?? 0; // a variavel recebe o valor utilizando o metodo post do formulario
+    $area = $largura * $comprimento; // formula da area armazenada em uma variavel
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,73 +23,46 @@
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
+
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Área do Terreno</h1>
 	</header>
+
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
+
+			<h2>Digite os valores:</h2>
+			<form action="index.php" method="post">
+
+				<label>Largura do terreno(m):
+					<input type="number" name="largura" required value="<?=$largura?>">
 				</label>
 
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
+				<label>Comprimento do terreno(m):
+					<input type="number" name="comprimento" required value="<?=$comprimento?>">
 				</label>
 
 				<button name="enviar"> Enviar </button>
 			</form>
 		</div>
-		<div class="box resposta">
-			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
-		</div>
+        <div class="box resposta">
+			<h2>Resultado</h2>
+			<?php
+
+                $metodo = $_SERVER["REQUEST_METHOD"]; //  dados do servidopr e da requisição que está ocorrendo no momento.
+
+                if($metodo == "POST"){ // verifica se o metodo é post e imrpime a mensagem na tela
+
+                    echo "<p class='alerta-verde'>A área do terreno é de {$area}m<sup>2</sup></p>";
+
+                }else{ // se não for post, é pq aguarda o envio do formulário
+
+                    echo "<p class='alerta-amarelo'>AGUARDANDO O ENVIO DO FORMULÁRIO</p>";
+
+                }
+
+            ?>
+		</div>		
 	</div>
 </body>
 </html>
