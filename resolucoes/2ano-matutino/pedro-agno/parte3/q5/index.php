db4736bd24edab1520c04c65a57045706e5ed313
pedroagno <114294264+pedroagno@users.noreply.github.com>
2023-04-29 10:29:50 -0300

    Questão 5 da parte 3 realizada, ok!

--- resolucoes/2ano-matutino/pedro-agno/parte3/q5/index.php
+++ resolucoes/2ano-matutino/pedro-agno/parte3/q5/index.php
@@ -8,59 +8,16 @@
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Descomplica Dividas</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
-				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
+			<h2>Informe:</h2>
+			<form action="resposta.php" method="POST">
+				<label>                              conta:
+				<input type="number" id="nome" name="conta" required>taxa:
+				<input type="number" id="nome" name="taxa" required>mensal:
+				<input type="number" id="nome" name="mensal" required>
 				</label>
 
 				<button name="enviar"> Enviar </button>
