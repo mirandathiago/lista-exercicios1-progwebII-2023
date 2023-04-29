f2fa8a6fb955821fcbd6a70a6d673e6ac514eac0
niculox <132169745+niculox@users.noreply.github.com>
2023-04-29 19:56:06 -0300

    Eu fiz a questão 4 da parte 3 da lista.

--- resolucoes/2ano-vespertino/nicolas/parte3/q4/index.php
+++ resolucoes/2ano-vespertino/nicolas/parte3/q4/index.php
@@ -1,79 +1,74 @@
+<?php
+
+    /*
+
+    4- Joãozinho está estudando matemática e precisa saber qual é o resultado da soma dos números de 1 a N. 
+    Crie um formulário que permita ao usuário informar um número inteiro N e, utilizando um laço de repetição do tipo for, 
+    calcule a soma dos números de 1 a N.
+
+    Exemplo de entrada:
+    N: 5
+    Exemplo de saída: Soma dos números de 1 a 5: 15
+
+    */
+
+    $n = $_POST["n"] ?? 0; // recebe formulario
+    $soma = 0; // declaração de variavel auxiliar
+
+		for($i = 1; $i <= $n; $i++) { // numero de vezes que ira compilar
+			$soma += $i; // soma de 1 ate n 
+		}
+		
+        $msg = "<p class='alerta-verde'>Soma dos números de 1 a {$n}: {$soma}</p>"; // mensagem
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Formulário</title>
+	<title>Questão 4</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
+
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Soma de 1 a N</h1>
 	</header>
+
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
+
+			<form action="index.php" method="post">
+
+				<label>Digite o valor de N:
+                <input type="number" name="n" min=1 step="1" required value="<?=$n?>"> 
 				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
-				</label>
-
-				<button name="enviar"> Enviar </button>
+                
+				<button name="enviar"> Calcular </button>
 			</form>
 		</div>
-		<div class="box resposta">
-			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
-		</div>
+
+        <div class="box resposta">
+			<h2>Resultado</h2>
+			<?php
+
+                $metodo = $_SERVER["REQUEST_METHOD"]; //  dados do servidopr e da requisição que está ocorrendo no momento.
+                
+                if($metodo == "POST"){ // verifica se o metodo é post e imrpime a mensagem na tela
+
+                    echo $msg; // imprime a mensagem (saída)
+
+                }
+                else{ // se não for post, é pq aguarda o envio do formulário
+
+                    echo "<p class='alerta-amarelo'>AGUARDANDO O ENVIO DO FORMULÁRIO</p>"; // mesnagem de aguardo ao envio do formulário
+
+                }
+
+            ?>
+		</div>		
 	</div>
 </body>
 </html>
