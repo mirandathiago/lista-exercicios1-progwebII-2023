8a9c1e03766a86caea32e07ad6d276216bee5b07
laura-LisboA <laurasantoa40@gmail.com>
2023-04-29 17:17:23 -0300

    Fiz a questão 1 da parte 1 da lista

---
+++ resolucoes/2ano-vespertino/laura-lisboa/parte1/q1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario</title>
+    <link href="estilo.css" rel="stylesheet">
+</head>		
+<body>
+<header>
+	<h1>Calculo do Preço de um Produto</h1>
+</header>    
+<div class="container">
+    <div class="box resposta">
+        <form action = "receber1.php" method = "post">
+            <label>Preço do Produto:
+                <input type="number" name="preco">
+            </label>
+            <br>
+            <button class="link">Enviar</button>
+        </form>
+    </div>
+</div>     
+</body>
+</html>
