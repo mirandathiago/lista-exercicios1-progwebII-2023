6c0141c5c9165ea2b9faaf0ba473746690f30bb7
Guilherme Nunes Cangussu <guilhermenunes3466@gmail.com>
2023-04-29 23:39:27 -0300

    Fiz a questão 2 da parte 3 da lista

---
+++ resolucoes/2ano-vespertino/guilherme/parte3/q2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Questão 2</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../../estilo.css">
+</head>
+<body>
+	<header>
+		<h1>TABUADA</h1>
+	</header>
+	<div class="container">
+		<div class="box formulario">
+			<h2>Entre com o valor</h2>
+			<form action="resposta.php" method="post">
+				<label>Valor:
+					<input type="text" step="1" name="n" required>
+				</label>
+
+				<button name="enviar"> Enviar </button>
+			</form>
+		</div>		
+	</div>
+</body>
+</html>
