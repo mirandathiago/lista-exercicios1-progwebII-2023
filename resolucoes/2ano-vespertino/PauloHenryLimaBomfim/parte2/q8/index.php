a9ef25175b640f4cd1fcbf90c7a9e13082b89195
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2023-04-30 09:49:28 -0300

    Resolução da questão 8

--- resolucoes/2ano-vespertino/PauloHenryLimaBomfim/parte2/q8/index.php
+++ resolucoes/2ano-vespertino/PauloHenryLimaBomfim/parte2/q8/index.php
@@ -1,9 +1,6 @@
 <?php
-    $nome = $_POST["nome"] ?? "";
-	$idade = $_POST["idade"] ?? "";
 	$peso = $_POST["peso"] ?? "";
 	$altura = $_POST["altura"] ?? "";
-	$sexo = $_POST["sexo"] ?? "Masculino";
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,27 +12,17 @@
 </head>
 <body>
 	<header>
-		<h1>Bom dia, boa tarde, boa noite, vá dormir</h1>
+		<h1>IMC Calculadora</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
 			<form arction="index.php" method="post">
-				<label>Entre com seu nome:
-					<input type="text" name="nome" required value="<?=$nome?>">
-				</label>
-				<label>Entre com sua idade:
-					<input type="number" name="idade" required value="<?=$idade?>">
-				</label>
 				<label>Entre com seu peso:
 					<input type="number" name="peso" required value="<?=$peso?>">
 				</label>
 				<label>Entre com sua altura:
-					<input type="number" name="altura" required value="<?=$altura?>">
+					<input type="number" name="altura" step="0.01" required value="1.50">
 				</label> 
-				<input type="radio" value="masculino" name="sexo"> Masculino 
-				<br> 
-				<input type="radio" value="feminino" name="sexo"> Feminino 
-				
 				<button> Enviar </button>
 			</form>
 		</div>
@@ -43,14 +30,27 @@
 			<h2>Resposta</h2>
 			<?php $metodo = $_SERVER["REQUEST_METHOD"];
 				if($metodo == "POST"){
-					if($sexo == "Masculino"){
-						$tmb = 88.36 + (13.4 * $peso) + (4.8 * $altura) - (5.7 * $idade);
+					$imc = $peso / ($altura*$altura);
+					if($imc < 18.5){
+						$status = "Você está abaixo do peso!";
 					}
-					else{
-						$tmb = 447.6 + (9.2 * $peso) + (3.1 * $altura) - (4.3 * $idade);
+					else if($imc >= 18.5 && $imc <= 24.9){
+						$status = "Você está com peso normal!";
+					}
+					else if($imc >= 25 && $imc <= 29.9){
+						$status = "Você está com sobrepeso!";
+					}
+					else if($imc >= 30 && $imc <= 34.9){
+						$status = "Você está com obesidade grau 1!";
+					}
+					else if($imc >= 35 && $imc <= 39.9){
+						$status = "Você está com obesidade grau 2!";
+					}
+					else if($imc >= 40){
+						$status = "Você está com obesidade grau 3!";
 					}
 					?>
-			<h2><?=$nome?>, sua taxa metabolica basal(TMB) é de aproximadamente <?=$tmb?> calorias por dia.</h2>
+			<h2>Seu imc é de <?=$imc?>, <?=$status?></h2>
 			<?php }else{
 				print("Envie o formulário!");
 			}?>
