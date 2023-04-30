77e1ab97accae5762b24a1965a89846255842891
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2023-04-29 21:23:19 -0300

    Resolução da questão 3

--- resolucoes/2ano-vespertino/PauloHenryLimaBomfim/parte2/q1/index.php
+++ resolucoes/2ano-vespertino/PauloHenryLimaBomfim/parte2/q1/index.php
@@ -1,3 +1,42 @@
+<?php
+    $num1 = $_POST["num1"] ?? "0";
+	$num2 = $_POST["num2"] ?? "0";
+	$num3 = $_POST["num3"] ?? "0";
+	$maior = $num1;
+	$menor = $num1;
+	if ($num1 >= $num2){
+		if($num1 >= $num3){
+			$maior = $num1;
+		}
+		else{
+			$maior = $num3;
+		}
+	}
+	else{
+		if($num2 >= $num3){
+			$maior = $num2;
+		}
+		else{
+			$maior = $num3;
+		}
+	}
+	if($num1 <= $num2){
+		if($num1 <= $num3){
+			$menor = $num1;
+		}
+		else{
+			$menor = $num3;
+		}
+	}
+	else{
+		if($num2 <= $num3){
+			$menor = $num2;
+		}
+		else{
+			$menor = $num3;
+		}
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,71 +47,26 @@
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Maior e menor de três números:</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
+			<form arction="index.php" method="post">
+				<label>Entre com um número:
+					<input type="number" name="num1" required value="<?=$num1?>">
 				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
+				<label>Entre com um número:
+					<input type="number" name="num2" required value="<?=$num2?>">
 				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
+				<label>Entre com um número:
+					<input type="number" name="num3" required value="<?=$num3?>">
 				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
-				</label>
-
-				<button name="enviar"> Enviar </button>
+				<button> Enviar </button>
 			</form>
 		</div>
 		<div class="box resposta">
 			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
+			<h2>Menor número: <?=$menor?> e o Maior número: <?=$maior?></h2>
 		</div>
 	</div>
 </body>
