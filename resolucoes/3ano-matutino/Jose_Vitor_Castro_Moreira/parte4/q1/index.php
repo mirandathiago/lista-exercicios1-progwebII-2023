e7cdb65b1f7a0edf08296d876ffb1fe5cc67a20d
José Vitor <castromoreirajosevitor372@gmail.com>
2023-04-30 15:25:48 -0300

    Resolução da questão 1

--- resolucoes/3ano-matutino/Jose_Vitor_Castro_Moreira/parte4/q1/index.php
+++ resolucoes/3ano-matutino/Jose_Vitor_Castro_Moreira/parte4/q1/index.php
@@ -4,75 +4,60 @@
 	<meta charset="UTF-8">
 	<title>Formulário</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="../../estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Frutas</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
-				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
+			<h2>Informe</h2>
+			<form action="index.php" method="post">
+				
 			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
+				<h3>Selecione suas frutas favoritas:</h3>
 				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
+				  <input type="checkbox" id="banana" name="frutas[]" value="banana">
 				  <label for="banana">Banana</label>
 				</div>
 				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
+				  <input type="checkbox" id="morango" name="frutas[]" value="morango">
 				  <label for="morango">Morango</label>
 				</div>
 				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
+				  <input type="checkbox" id="uva" name="frutas[]" value="uva">
 				  <label for="uva">Uva</label>
 				</div>
 				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
+				  <input type="checkbox" id="abacaxi" name="frutas[]" value="abacaxi">
 				  <label for="abacaxi">Abacaxi</label>
 				</div>
 				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
+				  <input type="checkbox" id="laranja" name="frutas[]" value="laranja">
 				  <label for="laranja">Laranja</label>
 				</div>
 			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
-				</label>
 
 				<button name="enviar"> Enviar </button>
 			</form>
 		</div>
 		<div class="box resposta">
-			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
+		<h2>Resultado</h2>
+		<?php
+			$metodo = $_SERVER["REQUEST_METHOD"];
+			
+			echo "<h3>As frutas selecionadas foram:</h3>";
+				if($metodo == "POST"){
+					$fruta = $_POST["frutas"];
+					foreach($fruta as $frutas){
+						echo $frutas;
+						echo "<br>";
+					}
+				}else{
+					echo"<p>Aguardando Operação</p>";}
+            ?>
+			<a href="index.php" class="link">Voltar</a>
 		</div>
 	</div>
 </body>
