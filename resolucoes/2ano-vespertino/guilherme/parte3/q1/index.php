8c493cee0ca98b9e541ab163dfedf9c216ae0455
Guilherme Nunes Cangussu <guilhermenunes3466@gmail.com>
2023-04-29 23:39:15 -0300

    Fiz a questão 1 da parte 3 da lista

---
+++ resolucoes/2ano-vespertino/guilherme/parte3/q1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Questão 1</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../../estilo.css">
+</head>
+<body>
+	<header>
+		<h1>FATORIAL</h1>
+	</header>
+	<div class="container">
+		<div class="box formulario">
+			<h2>Insira o número</h2>
+			<form action="resposta.php" method="get">
+				<label>Número:
+					<input type="number"step="1" name="num" required>
+				</label>
+
+				<button name="enviar"> Enviar </button>
+			</form>
+		</div>		
+	</div>
+</body>
+</html>
