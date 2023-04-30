fe38cf87afe6541975f733635d8a26acab95c7d1
isabelaSilvaPereira <isabeladasilvapereira1615@gmail.com>
2023-04-30 00:01:49 -0300

    Fiz a questão 1 da parte 2 da lista

---
+++ resolucoes/2ano-vespertino/isabela/parte2/q1/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Questão1</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<header>
+		<h2>Formulário para o maior número</h1>
+		<!-- criei o form utilizando o método post e enviando  o preço informado para o php-->
+	</header>
+	<div class="container">
+		<div class="box formulario">
+			<h2>Entre em contato</h2>
+			<form action="resposta.php" method="post">
+				<label>Informe o primeiro número:
+					<input type="number" id="num1" name="num1" required>
+				</label>
+				<label>Informe o segundo número:
+					<input type ="number" id="num2" name="num2" required> 
+				</label>
+				<label>Informe o terceiro número:
+					<input type ="number" id="num3" name="num3" required> 
+				</label>
+				<button name="enviar"> Enviar </button>
+			</form>
+		</div>		
+	</div>
+</body>
+</html>
