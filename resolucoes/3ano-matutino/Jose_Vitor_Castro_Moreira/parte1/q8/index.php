99682e82754175e915c8ad83b620f0398b36611f
José Vitor <castromoreirajosevitor372@gmail.com>
2023-04-30 00:31:40 -0300

    Resolução da questão 8

--- resolucoes/3ano-matutino/Jose_Vitor_Castro_Moreira/parte1/q8/index.php
+++ resolucoes/3ano-matutino/Jose_Vitor_Castro_Moreira/parte1/q8/index.php
@@ -1,78 +1,53 @@
+<?php
+	$valor = $_GET["valor"] ?? 0;
+	$parcela = $_GET["parcela"] ?? 0;
+	$juros = (($valor * 4) * $parcela) / 100;
+	$montante = $juros + $valor;
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Formulário</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="../../estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Calculadora de Juros</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
+			<h2>Informe</h2>
+			<form action="index.php" method="get">
+				<label>Valor do celular:
+					<input type="number" id="number" name="valor" step="0.01" min="0"  required value="<?=$valor?>">
 				</label>
 
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
+				<label>Quantidade de parcelas:
+					<input type="number" id="number" name="parcela"  min="0"  required value="<?=$parcela?>">
 				</label>
 
 				<button name="enviar"> Enviar </button>
 			</form>
 		</div>
 		<div class="box resposta">
-			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
+			<h2>Valor Total</h2>
+			<?php
+				$metodo = $_SERVER["REQUEST_METHOD"];
+
+				if($metodo == "GET" && isset($_GET["enviar"])){
+					if($parcela == 0){
+						echo "O celular será pago à vista pelo valor de R$ {$montante}<br>";
+					}else{
+						$mensal = round($montante / $parcela, 2);
+						echo "O celular será pago em {$parcela} vezes, no valor de R$ {$mensal}. <br> Totalizando R$ {$montante}<br>";
+					}
+				}else{
+					echo "Aguardando Informação";
+				}
+			?>
+			 <a href="" class="link">Voltar</a>
 		</div>
 	</div>
 </body>
