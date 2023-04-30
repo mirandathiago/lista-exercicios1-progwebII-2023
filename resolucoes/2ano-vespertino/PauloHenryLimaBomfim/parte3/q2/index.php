75d01b50a6c2c693feb80b10d23d9f3c02ad35d6
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2023-04-30 10:01:24 -0300

    Resolução da questão 2 P3

--- resolucoes/2ano-vespertino/PauloHenryLimaBomfim/parte3/q2/index.php
+++ resolucoes/2ano-vespertino/PauloHenryLimaBomfim/parte3/q2/index.php
@@ -1,3 +1,7 @@
+<?php
+	$num1 = $_POST["num1"] ?? "";
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,71 +12,32 @@
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Tabuada</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
-				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
-				</label>
-
-				<button name="enviar"> Enviar </button>
+			<form arction="index.php" method="post">
+				<label>Entre com um número:
+					<input type="number" name="num1" required value="<?=$num1?>">
+				</label> 
+				<button> Enviar </button>
 			</form>
 		</div>
 		<div class="box resposta">
 			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
+			<?php $metodo = $_SERVER["REQUEST_METHOD"];
+				if($metodo == "POST"){
+					$i = 0;
+					while( $i <= 10){
+						$tab = $num1 * $i;
+						print "{$num1} x {$i} = {$tab}<br>";
+						$i++;
+					}
+					
+				}
+				else{
+				print("Envie o formulário!");
+				}?>
 		</div>
 	</div>
 </body>
