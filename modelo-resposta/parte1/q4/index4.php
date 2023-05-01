13817d9e1b54110459b42a855c58705314c04b12
Joseph10101 <josepedrobibiano@gmail.com>
2023-05-01 12:26:23 -0300

    Resolução - Questão 4
    Resolução da Questão 4

---
+++ modelo-resposta/parte1/q4/index4.php
@@ -0,0 +1,44 @@
+<?php
+	$dist = $_GET["dist"] ?? 0;
+	$cons = $_GET["cons"] ?? 1;
+
+	$combustivel = $dist/$cons;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Questão 4</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<header>
+		<h1>Combustível</h1>
+	</header>
+	<div class="container">
+		<div class="box formulario">
+			<h2>Calculadora de Combustível</h2>
+			<br>
+			<form action="index4.php" method="get">
+				<label>Distância a ser percorrida (em km):
+					<input type="number" id="nome" name="dist" min="0" step="0.1" required>
+				</label>
+
+				<label>Consumo médio de combustível (em km/l):
+					<input type="number" id="nome" name="cons" min="0" step="0.1" required>
+				</label>
+
+				<button name="enviar"> Enviar </button>
+			</form>
+		</div>
+		<div class="box resposta">
+			<h2>Resposta</h2>
+			<br>
+			<p><?="Para percorrer uma distância de {$dist}km com consumo de {$cons}km/l é necessário"?></p>
+			<p class="alerta-verde"><?=$combustivel, " litros de combustível!"?></p>
+		</div>
+	</div>
+</body>
+</html>
