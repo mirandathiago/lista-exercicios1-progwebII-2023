6100a694addf7cc21a2a479e64a93722650ddcab
Joseph10101 <josepedrobibiano@gmail.com>
2023-05-01 11:40:46 -0300

    Resolução - Questão 1
    Resolução da Questão 1

---
+++ modelo-resposta/parte1/q1/index1.php
@@ -0,0 +1,37 @@
+<?php
+	$valor = $_GET["valor"];
+	$valor -= 0.1*$valor;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Questão 1</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<header>
+		<h1>Cálculo de Desconto</h1>
+	</header>
+	<div class="container">
+		<div class="box formulario">
+			<h2>Desconto de 10%</h2>
+			<form action="index1.php" method="get">
+				<label>Valor:
+					<input type="number" id="nome" name="valor" step="0.01" required>
+				</label>
+
+				<button name="enviar"> Enviar </button>
+			</form>
+		</div>
+		<div class="box resposta">
+			<h2>Valor Final</h2>
+			<br>
+			<p><?="O valor final do seu produto com desconto de 10% é de "?></p>
+			<p class="alerta-verde"><?="R$",number_format($valor,2,",",".")?></p>
+		</div>
+	</div>
+</body>
+</html>
