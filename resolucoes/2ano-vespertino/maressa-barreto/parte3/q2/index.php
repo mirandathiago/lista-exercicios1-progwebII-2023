c54f40c1f16265028496d7570f272f5060ce25fe
Maressa <maressabarreto0@gmail.com>
2023-05-01 19:11:55 -0300

    Resolução da questão 2

---
+++ resolucoes/2ano-vespertino/maressa-barreto/parte3/q2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!-- Desenvolva um programa que receba como entrada um número inteiro e calcule a sua tabuada de multiplicação utilizando o comando WHILE. Exiba a tabuada na tela.
+Entrada: 7
+Saída: 
+Tabuada do número 7:
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+-->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Questão 2</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<header>
+		<h2>Formulário para descoberta da tabuada</h1>
+		<!-- criei o form utilizando o método post e enviando  o preço informado para o php-->
+	</header>
+	<div class="container">
+		<div class="box formulario">
+			<h2>Tabuada</h2>
+			<form action="resposta.php" method="post">
+				<label>Informe o número interiro:
+					<input type="number" id="num1" name="num1" required>
+				</label>
+				<button name="enviar"> Enviar </button>
+			</form>
+		</div>		
+	</div>
+</body>
+</html>
