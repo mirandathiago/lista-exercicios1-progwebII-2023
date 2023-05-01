54e510a77cc605aaa94a5381ebcf5af2ae323297
Maressa <maressabarreto0@gmail.com>
2023-05-01 18:57:41 -0300

    Resolução da questão 2

--- resolucoes/2ano-vespertino/maressa-barreto/parte2/q2/index.php
+++ resolucoes/2ano-vespertino/maressa-barreto/parte2/q2/index.php
@@ -1,34 +1,3 @@
-<?php 
-
-$op = $_POST["selecao"] ?? '1' ;
-
-switch ($op) {
-
-case "1":
-$resposta = "Com o clima ensolarado, recomendamos ir ao Clube.";
-break;
-
-case "2":
-$resposta = "Com o clima Ameno, recomendamos ir ao Parque.";;
-break;
-
-case "3":
-$resposta = " Com o clima Chuvoso, recomendamos ir ao Cinema.";
-break;
-
-case "4":
-$resposta = " Com o clima Nublado, recomendamos ir ao Museu.";
-break ;
-
-case "5":
-$resposta = "Ta tempestuoso então fique em casa maluco .";
-break;
-
-
-}
-
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -46,21 +15,14 @@ break;
 
 		<div class="box formulario">
 
-				<form action="index.php" method="Post">
+			<h2>Calculadora</h2>
 
-				<label>Seleção o clima do momento :
-				<select name="selecao">
+			<form action="index.php" method="post">
 
-						<option <?= $op ==" " ? "selected" : ""?>>     </option>
-
-						<option  <?= $op =="1" ? "selected" : ""?> value="1">Ensolarado</option>
-						<option  <?= $op =="2" ? "selected" : ""?> value="2" >Ameno</option>
-						<option  <?= $op =="3" ? "selected" : ""?>  value="3">Chuvoso</option>
-						<option  <?= $op =="4" ? "selected" : ""?> value="4">Nublado</option>
-						<option  <?= $op =="5" ? "selected" : ""?> value="5">Tempestuoso</option>			
-					</select>
+				<label>consumo de energia elétrica :
+					<input type="number" name="num1"  step="0.1" required  >
 				</label>
-
+			
 				<button name="enviar"> Enviar </button>
 
 			</form>
@@ -69,10 +31,31 @@ break;
 		<div class="box resposta">
 
 			<h2>Resultado</h2>
+
+		<?php 
+
+		$num1 = $_POST["num1"] ?? 20;
 		
-					<h3> <?=$resposta?></h3>
+			if ($num1 > 20 && $num1 <= 100) {
+				$calculo = $num1 * 0.50;
+			}elseif ($num1 >= 101 && $num1 <=200) {
+				$calculo = $num1 * 0.70;
+			}elseif ($num1 > 200) {
+				$calculo = $num1 * 0.87;
+			}else{
+				$calculo = 20;
+			}
+	
+
+		?>
+
+		<h3> consumo de energia elétrica : <?=$num1?>KW/H </h3>
+		<br>
+		<h3>valor a ser pago  :<?=$calculo?></h3>
             
 		</div>
 	</div>
 </body>
 </html>
+
+
