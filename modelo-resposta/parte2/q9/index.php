0f69565f99b7b5aeecdab2af5068bb4f38e2237b
Joseph10101 <josepedrobibiano@gmail.com>
2023-05-01 20:12:00 -0300

    Resolução - Questão 9 - Parte 2
    Resolução da questao 9 da parte 2

--- modelo-resposta/parte2/q9/index.php
+++ modelo-resposta/parte2/q9/index.php
@@ -1,78 +1,62 @@
+<?php
+	$clima = $_GET["clima"] ?? "";
+
+	switch($clima){
+		case 1:
+			$mensagem = "Com o clima ensolarado, recomendamos ir ao Clube!";
+			break;
+		case 2:
+			$mensagem = "Com o clima ameno, recomendamos ir ao Parque!";
+			break;
+		case 3:
+			$mensagem = "Com o clima chuvoso, recomendamos ir ao Cinema!";
+			break;
+		case 4:
+			$mensagem = "Com o clima nublado, recomendamos ir ao Museu!";
+			break;
+		case 5:
+			$mensagem = "Com o clima tempestuoso, recomendamos ficar em Casa!";
+			break;
+		default:
+			$mensagem = "Recomendamos você selecionar uma opção de clima!";
+			break;
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Formulário</title>
+	<title>Questão 9</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Para onde vou?</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
-				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
-				</label>
+			<h2>Clima</h2>
+			<br>
+			<form action="index.php" method="get">
+			<label>Selecione o clima atual:</label>
+			<select name="clima">
+				<option value="">Escolha um clima</option>
+				<option value="1">Ensolarado</option>
+				<option value="2">Ameno</option>
+				<option value="3">Chuvoso</option>
+				<option value="4">Nublado</option>
+				<option value="5">Tempestuoso</option>
+			</select>
 
 				<button name="enviar"> Enviar </button>
 			</form>
 		</div>
 		<div class="box resposta">
-			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
+			<h2>Sugestão</h2>
+			<br>
+			<p class="alerta-verde"><?=$mensagem?></p>
 		</div>
 	</div>
 </body>
