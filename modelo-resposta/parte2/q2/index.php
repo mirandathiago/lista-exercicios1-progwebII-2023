25775ab3158df62e7addbb165099ca16f422a055
Joseph10101 <josepedrobibiano@gmail.com>
2023-05-01 14:56:43 -0300

    Resolução - Questão 1 - Parte 2
    Resolução da questão 1 da parte 2 da lista

--- modelo-resposta/parte2/q2/index.php
+++ modelo-resposta/parte2/q2/index.php
@@ -1,78 +1,53 @@
+<?php
+	/* Como a questão informou que a taxa mínima é válida apenas para valores menores que R$20,00, supus, então, que os valores que 
+	* superarem não receberiam esse acréscimo.
+	*/
+
+	$n1 = $_GET["n1"] ?? 0;
+
+	if($n1 > 200){
+		$valor = $n1*0.87;
+		$class = "alerta-vermelho";
+	}else if($n1 >= 101 && $n1 <= 200){
+		$valor = $n1*0.70;
+		$class = "alerta-amarelo";
+	}else{
+		$valor = $n1*0.50;
+		$class = "alerta-verde";
+
+		if($valor < 20.00){
+			$valor = 20.00;
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Formulário</title>
+	<title>Questão 2</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Conta de Energia</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
-				</label>
-
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
-				</label>
-
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
+			<h2>Consumo</h2>
+			<form action="index.php" method="get">
+				<label>Consumo de Energia Elétrica (em kWh):
+					<input type="number" id="nome" name="n1" required>
 				</label>
 
 				<button name="enviar"> Enviar </button>
 			</form>
 		</div>
 		<div class="box resposta">
-			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
+			<h2>Valor</h2>
+			<p>O valor a ser pago de acordo com o seu consumo é de </p>
+			<p class="<?=$class?>"><?="R$", number_format($valor,2,",",".")?></p>
 		</div>
 	</div>
 </body>
