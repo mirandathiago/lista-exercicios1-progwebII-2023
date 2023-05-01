debfdc8e2566e719cca94f29ccc6594e0d9d8592
Joseph10101 <josepedrobibiano@gmail.com>
2023-05-01 15:41:50 -0300

    Resolução - Questão 4 - P2
    Resolução da Questão 4 - Parte 2

--- modelo-resposta/parte2/q4/index.php
+++ modelo-resposta/parte2/q4/index.php
@@ -1,78 +1,62 @@
+<?php
+	$a = $_GET["a"] ?? 1;
+	$b = $_GET["b"] ?? 0;
+	$c = $_GET["c"] ?? 0;
+
+	if($a == 0){
+		$mensagem = "O valor de A não pode ser 0, tente novamente!";
+	}else{
+		$delta = $b**2 - 4*$a*$c;
+
+	if($delta < 0){
+		$mensagem = "A equação não possui raízes reais.";
+	}else if($delta > 0){
+		$raiz1 = (-$b + sqrt($delta))/(2*$a);
+		$raiz2 = (-$b - sqrt($delta))/(2*$a);
+		$mensagem = "A equação possui duas raízes reais: {$raiz1} e {$raiz2}";
+	}else{
+		$raiz = (-$b + sqrt($delta))/(2*$a);
+		$mensagem = "A equação possui apenas uma raiz real: {$raiz}";
+	}
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Formulário</title>
+	<title>Questão 3</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 	<header>
-		<h1>Formulário de Contato</h1>
+		<h1>Programa Educado</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Entre em contato</h2>
-			<form>
-				<label>Nome:
-					<input type="text" id="nome" name="nome" required>
+			<h2>Coeficientes</h2>
+			<form action="index.php" method="get">
+				<label>Insira o valor de A:
+					<input type="number" id="nome" name="a" required>
 				</label>
 
-				<label>E-mail:
-					<input type="email" id="email" name="email" required>
+				<label>Insira o valor de B:
+					<input type="number" id="nome" name="b" required>
 				</label>
 
-				<label>Telefone:
-					<input type="tel" id="telefone" name="telefone" required>
-				</label>
-
-				<label>Assunto:
-					<select name="assunto">
-						<option></option>
-						<option value="1">Nota</option>
-						<option value="2">Trabalho</option>
-						<option value="3">Feriado</option>
-					</select>
-			   </label>
-
-			   <fieldset>
-				<legend>Selecione suas frutas favoritas:</legend>
-				<div>
-				  <input type="checkbox" id="banana" name="frutas" value="banana">
-				  <label for="banana">Banana</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="morango" name="frutas" value="morango">
-				  <label for="morango">Morango</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="uva" name="frutas" value="uva">
-				  <label for="uva">Uva</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas" value="abacaxi">
-				  <label for="abacaxi">Abacaxi</label>
-				</div>
-				<div>
-				  <input type="checkbox" id="laranja" name="frutas" value="laranja">
-				  <label for="laranja">Laranja</label>
-				</div>
-			  </fieldset>
-
-				<label>Mensagem:
-					<textarea id="mensagem" name="mensagem" required></textarea>
+				<label>Insira o valor de C:
+					<input type="number" id="nome" name="c" required>
 				</label>
 
 				<button name="enviar"> Enviar </button>
 			</form>
 		</div>
 		<div class="box resposta">
-			<h2>Resposta</h2>
-			<p>A resposta será exibida aqui.</p>
-			<p class="alerta-vermelho">Mensagem de alerta aqui</p>
-			<p class="alerta-verde">Mensagem de alerta aqui</p>
-			<p class="alerta-amarelo">Mensagem de alerta aqui</p>
-            <a href="" class="link">Voltar</a>
+			<h2>Raízes da Equação Quadrática</h2>
+			<br>
+			<p><strong>Equação: </strong><?="{$a}x² + {$b}x + {$c} = 0"?></p>
+			<p class="alerta-amarelo"><?=$mensagem?></p>
 		</div>
 	</div>
 </body>
