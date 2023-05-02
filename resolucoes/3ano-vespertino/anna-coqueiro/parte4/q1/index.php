b30d621c7d2ed01d941dae92b185916bd5d6320f
Anna Luisa <annaluisa.2090@gmail.com>
2023-05-01 21:13:40 -0300

    Acrescentei mais frutas à caixa de seleção

--- resolucoes/3ano-vespertino/anna-coqueiro/parte4/q1/index.php
+++ resolucoes/3ano-vespertino/anna-coqueiro/parte4/q1/index.php
@@ -13,28 +13,40 @@
 	<div class="container">
 		<div class="box formulario">
 			
-			<form action="exemplo2.php" method="get">
+			<form action="index.php" method="get">
 			  <fieldset>
-				<legend>Escolha suas frutas favoritas:</legend>
+				<legend>Escolha suas frutas preferidas:</legend>
 				<div>
-				  <input type="checkbox" id="banana" name="frutas[]" value="banana">
+				  <input type="checkbox" id="banana" name="frutas[]" value="Banana">
 				  <label for="banana">Banana</label>
 				</div>
 				<div>
-				  <input type="checkbox" id="morango" name="frutas[]" value="morango">
+				  <input type="checkbox" id="morango" name="frutas[]" value="Morango">
 				  <label for="morango">Morango</label>
 				</div>
 				<div>
-				  <input type="checkbox" id="uva" name="frutas[]" value="uva">
+				  <input type="checkbox" id="uva" name="frutas[]" value="Uva">
 				  <label for="uva">Uva</label>
 				</div>
 				<div>
-				  <input type="checkbox" id="abacaxi" name="frutas[]" value="abacaxi">
+				  <input type="checkbox" id="abacaxi" name="frutas[]" value="Abacaxi">
 				  <label for="abacaxi">Abacaxi</label>
 				</div>
 				<div>
-				  <input type="checkbox" id="laranja" name="frutas[]" value="laranja">
+				  <input type="checkbox" id="laranja" name="frutas[]" value="Laranja">
 				  <label for="laranja">Laranja</label>
+				</div>
+				<div>
+				  <input type="checkbox" id="ameixa" name="frutas[]" value="Ameixa">
+				  <label for="ameixa">Ameixa</label>
+				</div>
+				<div>
+				  <input type="checkbox" id="melancia" name="frutas[]" value="Melancia">
+				  <label for="melancia">Melancia</label>
+				</div>
+				<div>
+				  <input type="checkbox" id="maca" name="frutas[]" value="Maçã">
+				  <label for="maca">Maçã</label>
 				</div>
 			  </fieldset>
 				
