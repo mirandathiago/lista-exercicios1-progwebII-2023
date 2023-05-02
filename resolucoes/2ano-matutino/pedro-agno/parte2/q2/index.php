82f3d4c3d05daaf4111a906431282ce7cc4f780a
pedroagno <114294264+pedroagno@users.noreply.github.com>
2023-05-01 23:03:00 -0300

    todas questões realizadas

--- resolucoes/2ano-matutino/pedro-agno/parte2/q2/index.php
+++ resolucoes/2ano-matutino/pedro-agno/parte2/q2/index.php
@@ -1,20 +1,3 @@
-
-<?php
-/*
-2- Seu João quer saber quanto vai ser a sua conta de energia no mês apenas olhando para o medidor de energia
- de sua residência. Para ajudá-lo crie um formulário que receba o consumo de energia elétrica em kWh
-  (quilowatts-hora). Em seguida, crie um código em PHP que calcule o valor da conta de energia com base
-   nas seguintes regras:
-Até 100 kWh, o valor é de R$ 0,50 por kWh.
-De 101 a 200 kWh, o valor é de R$ 0,70 por kWh.
-Acima de 200 kWh, o valor é de R$ 0,87 por kWh.
-Além disso, a conta de energia possui uma taxa mínima de R$ 20,00. Portanto, caso o valor calculado seja inferior a R$ 20,00, o valor a ser cobrado deve ser de R$ 20,00.
-Exemplo de entrada: Consumo de energia: 250 kWh
-Exemplo de saída: Valor da conta de energia: R$ 237,50
-
-*/
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -25,16 +8,19 @@ Exemplo de saída: Valor da conta de energia: R$ 237,50
 </head>
 <body>
 	<header>
-		<h1> consumo de energia elétrica em KWH</h1>
+		<h1>Conta de Energia</h1>
 	</header>
 	<div class="container">
 		<div class="box formulario">
-			<h2>Calculo o valor da Energia em KWH </h2>
+			<h2>Calcule aqui o valor de sua conta de Energia! </h2>
 			<form action="resposta.php" method="get">
-				<label>digite o consumo de energia:
-					<input type="number" id="consumo" name="consumo" >
+				<label>Infome aqui seu gasto em energia KWH:
+					<input type="number" id="dist" name="consumo" required>
 				</label>
+
 				<button name="enviar"> Enviar </button>
+			
+				<h2></h2>
 			</form>
 		</div>		
 	</div>
