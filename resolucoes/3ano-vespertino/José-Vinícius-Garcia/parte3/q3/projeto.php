625e9bd6828a4bd38cc063d0c5fdcda5ea3947b4
JoseViniss <joseviniss2005@gmail.com>
2023-05-03 05:30:57 -0300

    resolução da Q3 pt3

--- resolucoes/3ano-vespertino/José-Vinícius-Garcia/parte3/q3/projeto.php
+++ resolucoes/3ano-vespertino/José-Vinícius-Garcia/parte3/q3/projeto.php
@@ -1,44 +1,19 @@
-<?php
-/*3- João deseja saber quantos números pares existem em um intervalo. 
-Crie um formulário que permita ao usuário informar os dois números que delimitam o intervalo e, 
-utilizando um laço de repetição do tipo for, calcule a quantidade de números pares nesse intervalo.
-Exemplo de entrada:
-Início do intervalo: 3
-Fim do intervalo: 15
-
-
-*/
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-	<meta charset="UTF-8">
-	<title>Formulário</title>
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 3</title>
 </head>
 <body>
-	<header>
-		<h1>Contador</h1>
-	</header>
-	<div class="container">
-		<div class="box formulario">
-			<h2>Quantos números pares temos?</h2>
-			<form action="resposta.php" method="post">
-				<label>Número inicial
-					<input type="number" name="num1" required>
-				</label>
-
-				<label>Número final
-					<input type="number" name="num2" required>
-				</label>
-
-
-
-				<button name="enviar"> Enviar </button>
-			</form>
-		</div>		
-	</div>
+<h1>Cálculo de números pares</h1>
+    <form action="resposta.php" method="get">
+        <label for="num1">Início do intervalo:</label>
+        <input type="number" name="num1" id="num1">
+        <label for="num2">Fim do intervalo:</label>
+        <input type="number" name="num2" id="num2">
+        <input type="submit" value="Calcular">
+    </form>
 </body>
 </html>
